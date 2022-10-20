d4f06cbe6b7c6bbb1f3d3ba29dc49870e184e264
zobyerAppnap <ahmzobyer@appnap.io>
2022-10-20 15:28:48 +0600

    feat: sho page added

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -46,15 +46,6 @@ function ProductDetails() {
     <div>
       <TopBar />
       <NavBar />
-      <div className="container py-4 flex items-center gap-3">
-        <a href="../index.html" className="text-primary text-base">
-          <i className="fa-solid fa-house"></i>
-        </a>
-        <span className="text-sm text-gray-400">
-          <i className="fa-solid fa-chevron-right"></i>
-        </span>
-        <p className="text-gray-600 font-medium">Product</p>
-      </div>
 
       <div className="container grid grid-cols-2 gap-6">
         <div className="">
